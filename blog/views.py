8c95878299ad2929ccf5a1304113cad480d02cce
dzmitry <v.dmitryvasilyev@gmail.com>
2020-10-29 15:20:43 +0200

    g

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 # Create your views here.
-from django.views.generic import ListView, DeleteView
+from django.views.generic import ListView, DetailView
 from .models import Post
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 
 class BlogListView(ListView):
@@ -9,8 +11,31 @@ class BlogListView(ListView):
     template_name = 'home.html'
 
 
-class BlogDetailView(DeleteView):
+class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
+# Мы используем reverse_lazy, а не просто reverse, чтобы он не выполнял
+# перенаправление URL, пока наше представление не завершит удаление сообщения в блоге.
+
+
+
 
 
